0d8b9cf11590cdeb7b84eefc3f3c94f0361b33c4
Yogesh Gajjar <ygajjar@usc.edu>
2019-08-09 20:14:34 -0700

    added comments

--- README.md
+++ README.md
@@ -1,4 +1,7 @@
 # race_on
-1/10th size Autonomous car working code. 
+
+1/10th SIZE AUTONOMOUS CAR SOURCE CODE 
 
 The autonomous car consists of a servo motor, main motor, raspberry pi and raspberry pi camera for detecting lanes. The algorithm uses OpenCV for image processing. It detects peaks from the image and uses butterworth filter to filter out the peaks. The algorithm also uses PID for controlling the servo based on the peaks detected. 
+
+This code is for tracking the lane on which the car is moving such that the PID control will keep the car back on track. 
--- race_on.py
+++ race_on.py
@@ -1,30 +1,35 @@
+###########################################################################
+#    SOURCE CODE FOR MIDDLE LANE DETECTION USING 1/10th AUTONOMOUS CAR 
+###########################################################################
+
+# INITIAL DECLARATION OF LIBRARIES 
 from picamera.array import PiYUVArray, PiRGBArray
 from picamera import PiCamera
 from scipy.signal import find_peaks, butter, filtfilt
 import time
 import numpy as np
-from pwm import PWM
+from pwm import PWM  #PWM imported from another script. 
 
 
 # PWM1 is for servo motor which helps in turning of the car. 
-# Initialization of PWM and setting it facing straight. 
+# Initialization of PWM and making the front wheel facing front. 
+#1500000 gives centre value, 1000000 gives right turn & 2000000 give left turn
+
 pwm1 = PWM(1)
-print("PWM(1)")
 pwm1.export()
 pwm1.period = 20000000
 pwm1.duty_cycle = 1500000
 pwm1.enable = True
-print("servo ready")
 
-# PWM0 is for motors for moving the car
+# PWM0 is for main motor for moving the car. 
 # Initialization of PWM. 
+#1500000 gives centre value, 1000000 gives right turn & 2000000 give left turn
+
 pwm0 = PWM(0)
-print("PWM(0)")
 pwm0.export()
 pwm0.period = 20000000
 pwm0.duty_cycle = 1000000
 pwm0.enable = True
-print("Motor ready")
 time.sleep(5)
 
 def pwmservo(error,t1, pre_error):
@@ -35,8 +40,6 @@ def pwmservo(error,t1, pre_error):
     all_error = 0
     all_error = all_error + error
     output = Kp*error + Ki*all_error*t1 + Kd * (error - pre_error)  #calculating output with PID
-    print(int(round(output)))
-    print("Output", output)
     if error > 100:
         if (round(output)) > 500000:
             pwm1.duty_cycle = 1000000  #turns max left
@@ -57,12 +60,12 @@ def pwmservo(error,t1, pre_error):
         pwm1.enable = True
 
 
-def callmotoroff():
+def callmotoroff(): # To make the main motor stop
     pwm0.duty_cycle = 1000000
     pwm0.enable = False
     print("Motor off")
 
-def callservooff():
+def callservooff(): # To make the servo stop
     pwm1.duty_cycle = 1000000
     pwm1.duty_cycle = 1500000
     pwm1.enable = False
@@ -74,7 +77,7 @@ res = (640, 480)
 
 # To filter the noise in the image we use a 3rd order Butterworth filter
 
-# Wn = 0.02, the cut-off frequency, acceptable values are from 0 to 1
+# Wn = 0.007, the cut-off frequency, acceptable values are from 0 to 1
 b, a = butter(3, 0.007)
 camera = PiCamera()
 
@@ -88,15 +91,11 @@ camera.rotation = 180
 rawCapture = PiYUVArray(camera, size=res)
 stream = camera.capture_continuous(rawCapture, format="yuv", use_video_port=True)
 
-# Measure the time needed to process 300 images to estimate the FPS
+# Measure the time needed to process 550 images to estimate the FPS
 N = 550
 k = 0
 t = time.time()
-pre_xmax = 0
-time_elapsed = 0
-t1 = 0
-pre_error = 0
-print("Start")
+pre_xmax, time_elapsed, t1, pre_error = 0
 for f in stream:
     # Get the intensity component of the image (a trick to get black and white images)
     I = f.array[:, :, 0]
@@ -116,6 +115,7 @@ for f in stream:
     ymax = np.max(Lf)
     ymin = np.min(Lf)
 
+    # Identifying error from the peaks
     if (ymax - ymin > 50):
         error = 320 - xmax
         pre_xmax = xmax
@@ -142,7 +142,6 @@ callservooff()
 stream.close()
 rawCapture.close()
 camera.close()
-
 callmotoroff()
 pwm1.duty_cycle = 1500000
 print("All Done")
